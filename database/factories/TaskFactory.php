bb14bfb4dfb69353b994d2e3736d4ff637b9af5a
Souvik Das <souvikdas.dev@gmail.com>
2024-06-15 17:16:33 +0530

    Added Task factory & seeder

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'description' => fake()->sentence(),
+            'due_date' => fake()->date(),
+            'priority' => fake()->randomDigit(),
+            'status' => fake()->randomElement(['Pending', 'Complete', 'Incomplete']),
+        ];
+    }
+}
