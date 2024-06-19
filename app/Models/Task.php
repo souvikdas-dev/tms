122dca61e1f69529b4f86223dace0191f5d1d86a
Souvik Das <souvikdas.dev@gmail.com>
2024-06-19 09:13:25 +0530

    Added due_date cast in Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'due_date' => 'date',
+    ];
 }
