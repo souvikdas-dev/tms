904f70c47fcd65f40291cc1afeeb8ccfd1de04dd
Sambhu Baroi <mrbaroi839@gmail.com>
2024-10-29 21:22:47 +0530

    change alert component design

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,25 +1,57 @@
-@props(['type'])
+@props(['type' => 'info', 'title'=> '', 'message'])
 
 @php
-$color = 'text-blue-800 bg-blue-50';
-if($type == 'success'){
-$color = 'text-green-800 bg-green-50';
-}
-elseif($type == 'error'){
-$color = 'text-red-800 bg-red-50';
-}
-elseif($type == 'warning'){
-$color = 'text-yellow-800 bg-yellow-50';
-}
+
+$alertClasses = [
+    'info' => 'text-blue-800 bg-blue-50',
+    'success' => 'text-green-800 bg-green-50',
+    'warning' => 'text-yellow-800 bg-yellow-50',
+    'error' => 'text-red-800 bg-red-50',
+][$type];
+
+$closeBtnClasses = [
+    'info' => 'bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200',
+    'success' => 'bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200',
+    'warning' => 'bg-yellow-50 text-yellow-500 rounded-lg focus:ring-2 focus:ring-yellow-400 p-1.5 hover:bg-yellow-200',
+    'error' => 'bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200',
+][$type];
+
 @endphp
 
 
-<div class="flex items-center p-4 mb-4 text-sm rounded-lg {{ $color }}" role="alert">
-    <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+<div class="flex items-center p-4 mb-4 gap-3 text-sm rounded-lg {{ $alertClasses }}" role="alert">
+    <!-- alert icon -->
+    @if($type == 'success')
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="inline-block w-6">
+        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
     </svg>
+    @elseif($type == 'warning')
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="inline-block w-6">
+        <path fill-rule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd" />
+    </svg>
+    @elseif($type == 'error')
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="inline-block w-6">
+        <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clip-rule="evenodd" />
+    </svg>
+    @elseif($type == 'info')
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="inline-block w-6">
+        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd" />
+    </svg>
+    @endif
+
     <span class="sr-only">{{ $type }}</span>
+
     <div>
-        <span class="font-medium">Success alert! - type: {{ $type }}</span> Change a few things up and try submitting again.
+        @if($title)
+        <span class="font-medium">{{ $title }}</span> <br/>
+        @endif
+        {{ $message }}
     </div>
+
+    <button type="button" onclick="this.parentElement.style.display = 'none';" class="ms-auto -mx-1.5 -my-1.5 inline-flex items-center justify-center h-8 w-8 {{ $closeBtnClasses }}" data-dismiss="alert" aria-label="Close">
+        <span class="sr-only">Close</span>
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+        </svg>
+    </button>
 </div>
